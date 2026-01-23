ac6297d008be64c76098fd19069439138dd5b398
BrivaelFer <77669345+BrivaelFer@users.noreply.github.com>
2026-01-23 13:48:46 +0100

    meteo

--- src/Controller/MeteoController.php
+++ src/Controller/MeteoController.php
@@ -6,44 +6,90 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\User;
-use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 final class MeteoController extends AbstractController
 {
 
+    private string $apiUrl;
+    private string $apiKey;
+
     public function __construct(
-        UserRepository $userRepository,
-        EntityManagerInterface $entityManager,
-        HttpClientInterface $httpClient
     )
     {
-        
+        $this->apiUrl = $_ENV['METEO_URL'];
+        $this->apiKey = $_ENV['METEO_KEY'];
     }
 
     #[Route('/meteo', name: 'app_meteo')]
-    public function getUserMeteo(): JsonResponse
+    public function getUserMeteo(HttpClientInterface $httpClient, TagAwareCacheInterface $tagAwareCacheInterface): JsonResponse
     {
         /** @var User */
         $user = $this->getUser();
+        $ville = $user->getVille();
 
-        return $this->apiCall($user->getVille());
+        $data = $this->getData($tagAwareCacheInterface, $httpClient, $ville);
+
+        if($data['code'] == 404) {
+            $data['content'] = [
+                'erreur' => 404,
+                'message' => 'Aucune données trouver pour la ville de l\'utilisateur'
+            ];
+        }
+
+        return $this->json($data['content'], $data['code']);
     }
 
     #[Route('/meteo/{ville}', name: 'app_meteo_ville')]
-    public function getvilleMeteo(string $ville): JsonResponse
+    public function getvilleMeteo(string $ville, HttpClientInterface $httpClient, TagAwareCacheInterface $tagAwareCacheInterface): JsonResponse
     {
-        return $this->apiCall($ville);
+        $data = $this->getData($tagAwareCacheInterface, $httpClient, $ville);
+
+        if($data['code'] == 404) {
+            $data['content'] = [
+                'erreur' => 404,
+                'message' => 'Aucune données trouver pour la ville : ' . $ville
+            ];
+        }
+
+        return $this->json($data['content'], $data['code']);
     }
 
-    private function apiCall(string $ville): JsonResponse
+    private function getData(TagAwareCacheInterface $tagAwareCacheInterface, HttpClientInterface $httpClient, string $ville): array
     {
-        
+        $idCache = "meteo-{$ville}";
+        return $tagAwareCacheInterface->get($idCache, function (ItemInterface $item) use ($httpClient, $ville) {
+            $item->tag("meteoCache");
+            $item->expiresAfter(3600);
+            return $this->apiCall($ville, $httpClient);
+        });
+    }
 
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MeteoController.php',
+    private function apiCall(string $ville, HttpClientInterface $httpClient): array
+    {
+
+        $response = $httpClient->request('GET', $this->apiUrl, [
+            'query' => [
+                'q' => $ville,
+                'appid' => $this->apiKey,
+                'units' => 'metric',
+            ],
         ]);
+
+        $code = $response->getStatusCode();
+        if ($code == 404) {
+            return [
+                'code' => $code
+            ];
+        }
+
+        $content = json_decode($response->getContent(), true);
+
+        return [
+            'code' => $code,
+            'content' => $content
+        ];
     }
 }
