e76b96d9d522e83fed1acd916b42ef32b2f5ea45
BrivaelFer <77669345+BrivaelFer@users.noreply.github.com>
2026-01-30 14:05:59 +0100

    user controller end

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,45 +7,93 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final class UserController extends AbstractController
 {
-    public function __construct(
-        UserRepository $userRepository,
-        EntityManagerInterface $entityManager
-    )
-    {
-        
-    }
+    
     
     #[Route('/user', name: 'api_create_user', methods: 'POST')]
-    public function createUser(): JsonResponse
+    public function createUser(
+        Request $request, 
+        SerializerInterface $serializer, 
+        EntityManagerInterface $em, 
+        UserPasswordHasherInterface $passwordHasher,
+        ValidatorInterface $validator
+    ): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller POST!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+
+        $errors = $validator->validate($user);
+
+        if($errors->count() > 0) {
+            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+        }
+        
+        $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+        
+        $em->persist($user);
+        $em->flush();
+
+        return $this->json(['message' => 'user enregistré'], 200);
     }
 
     #[IsGranted('ROLE_ADMIN')]
     #[Route('user/{id}', name: 'api_put_user', methods: 'PUT')]
-    public function updateUser(User $user): JsonResponse
+    public function updateUser(
+        User $user,
+        Request $request, 
+        SerializerInterface $serializer, 
+        EntityManagerInterface $em, 
+        UserPasswordHasherInterface $passwordHasher,
+        ValidatorInterface $validator
+    ): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller DELETE!',
-            'path' => 'src/Controller/UserController.php',
+        $currentPw = $user->getPassword();
+        $id = $user->getId();
+
+        /** @var User */
+        $requestUser = $serializer->deserialize($request->getContent(), User::class, 'json', [
+            AbstractNormalizer::OBJECT_TO_POPULATE => $user
         ]);
+
+        $errors = $validator->validate($requestUser);
+
+        if($errors->count() > 0) {
+            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+        }
+
+        if($currentPw !== $requestUser->getPassword()) {
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+        }
+
+        $em->persist($requestUser);
+        $em->flush();
+
+        return $this->json([
+            'code' => 200,
+            'message' => 'Utilisater ' . $id . ' modifié',
+        ], 200);
     }
 
     #[IsGranted('ROLE_ADMIN')]
     #[Route('user/{id}', name: 'api_del_user', methods: 'DELETE')]
-    public function deleteUser(User $user): JsonResponse
+    public function deleteUser(User $user, EntityManagerInterface $em): JsonResponse
     {
+        $email = $user->getEmail();
+
+        $em->remove($user);
+        $em->flush();
+
         return $this->json([
-            'message' => 'Welcome to your new controller DELETE!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+            'code' => 200,
+            'message' => 'Utilisateur ' . $email . ' supprimé avec succès',
+        ], 200);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,7 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
@@ -18,6 +19,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
+    #[Assert\NotBlank(message: "Doit être renseigné")]
+    #[Assert\Email(message: "Doit être un email")]
     private ?string $email = null;
 
     /**
@@ -30,9 +33,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @var string The hashed password
      */
     #[ORM\Column]
+    #[Assert\NotBlank(message: "Doit être renseigné")]
     private ?string $password = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "Doit être renseigné")]
     private ?string $ville = null;
 
     public function getId(): ?int
